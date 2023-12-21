7c82b24c79beed1f94f324c23e7e2db76cb2f412
HuSTLeR1709 <nabhjeetkatiyar3@gmail.com>
2023-12-21 21:04:40 +0530

    database added

---
+++ Controllers/auth.js
@@ -0,0 +1,14 @@
+const bcrypt = require("bcrypt"); 
+const User = require('../models/User')
+
+
+exports.signup = async (req, res)=>{
+    try {
+        const {name , email, password, role} = req.body;
+        
+        
+    } catch (error) {
+        
+    }
+
+}
