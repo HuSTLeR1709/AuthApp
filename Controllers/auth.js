65fb2eb66e2e7c54167f6d7ba917a32e420f9b1a
HuSTLeR1709 <nabhjeetkatiyar3@gmail.com>
2023-12-24 19:43:31 +0530

    signuphandeler created

--- Controllers/auth.js
+++ Controllers/auth.js
@@ -5,9 +5,45 @@ const User = require('../models/User')
 exports.signup = async (req, res)=>{
     try {
         const {name , email, password, role} = req.body;
-        
+
+        const existinguser = await User.findOne({email});
+
+        if(existinguser){
+            return res.status(400).json({
+                success:false,
+                message:"User already registered"
+            });
+        }
+
+        let hashedpassword;
+        try{
+            hashedpassword =await bcrypt.hash(password, 10);
+        }
+        catch(error){
+            return res.status(500).json({
+                success:false,
+                message:"Error in password hashing",
+            })
+
+        }
+
+        const user = await User.create({
+            name, email, password:hashedpassword, role
+        });
+
+        res.status(200).json({
+            success:true,
+            message:"User registered successfully",
+        })
+
         
     } catch (error) {
+
+        console.log(error);
+        return res.status(400).json({
+            success:false,
+            message:"user can not be registered",
+        })
         
     }
 
